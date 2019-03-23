2f96e17994d6ef37b7f7358d8eb5b68d76b5aad5
Konsta <konsta09@gmail.com>
2019-03-23 19:38:04 +0200

    Revert "drivers/gpu/drm: changes for android"
    This reverts commit 0a3e57cfd896ac48fb079b74e97679fbedd1edfb.

--- drivers/gpu/drm/drm_file.c
+++ drivers/gpu/drm/drm_file.c
@@ -218,7 +218,7 @@ static int drm_open_helper(struct file *filp, struct drm_minor *minor)
 	priv->minor = minor;
 
 	/* for compatibility root is always authenticated */
-	priv->authenticated = 1;
+	priv->authenticated = capable(CAP_SYS_ADMIN);
 	priv->lock_count = 0;
 
 	INIT_LIST_HEAD(&priv->lhead);
